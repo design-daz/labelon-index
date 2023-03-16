b8aef73a502eca32b7ed29e2a18c7393816cd38c
Daz <90227415+design-daz@users.noreply.github.com>
2023-03-16 09:46:26 +0900

    230316

--- js/script.js
+++ js/script.js
@@ -61,7 +61,29 @@ $(document).ready(function () {
       prevEl: ".swiper-button-prev.sec2-sld-p",
       nextEl: ".swiper-button-next.sec2-sld-n",
     },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      360: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
   });
+
+
+  
 
   // [2.0.0] vetical tab
   // =========================== //
